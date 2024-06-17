7930263af4aef62cd08060c724bcfaee3c7d13dd
Saijeet <saijeetchitte@gmail.com>
2024-06-17 14:08:54 +0530

    hi

---
+++ index.js
@@ -0,0 +1,45 @@
+const express =require('express')
+const users= require("./MOCK_DATA.json")
+
+const app=express();
+
+const PORT=8000;
+
+// Routes
+app.get("/users",(req,res)=>{
+    const html =`
+    <ul> 
+    ${users.map((user)=>`<li>${user.first_name}</li>`
+    ).join("")}
+
+    </ul>
+    `;
+    res.send(html);
+});
+
+app.get("/api/users",(req,res)=>{
+    return res.json(users);
+});
+
+app.get("/api/users/:id",(req,res)=>{
+    const id=Number(req.params.id);
+    const user=users.find((user)=>user.id ===id);
+    return res.json(user);
+});
+
+app.post("/api/users",(req,res) =>{
+    return res.json({status:"pending"});
+});
+
+app.patch("/api/users/:id",(req,res) =>{
+    return res.json({status:"pending"});
+});
+
+app.post("/api/users",(req,res) =>{
+    return res.json({status:"pending"});
+});
+
+
+
+
+app.listen(PORT,() => console.log('Server started at PORT:${PORT}'))
